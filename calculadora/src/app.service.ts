61e834000c04d674441574027b53d0a452d90ca7
Kevin-Villacis <kevinxdmafla@gmail.com>
2025-05-16 09:11:50 -0500

    hasta Later - Test(e2e) e inicio de docker

--- calculadora/src/app.service.ts
+++ calculadora/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Console } from 'console';
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log('log!');
+    console.debug('debug!');
+    console.info('info!');
+    console.warn('warn!');
+    console.error('error!');
     return 'Hello World!';
   }
 }
